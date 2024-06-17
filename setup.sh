5d7c848b65be972ba1abb400261b2f33532dad86
Karthik Ganesan <Karthikganesan17@gmail.com>
2024-06-17 11:19:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
-./Miniconda-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
 conda create -n cyborg-env python=3.11
 conda activate cyborg-env
 pip install -r requirements.txt
