88e387ef072b4de1b5e7b90f524c439fe4c61697
Karthik Ganesan <Karthikganesan17@gmail.com>
2024-06-17 11:17:58 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda-latest-Linux-x86_64.sh
+conda create -n cyborg-env python=3.11
+conda activate cyborg-env
+pip install -r requirements.txt
+wget https://github.com/facebookresearch/AudioDec/releases/download/pretrain_models_v02/exp.zip
+apt-get update
+apt-get install unzip
+unzip exp.zip
